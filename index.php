e106af522fe3d7b2be9785810a2c0b25e84704f9
root <root@DESKTOP-B3KDVC4.localdomain>
2020-05-17 13:24:17 +0200

    add styles

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello wuja</title>
-    <link rel="stylesheet" type="text/css" href="styles/index.css">
+    <link rel="stylesheet" type="text/css" href="/styles/index.css">
 </head>
 <body id="main">
     <p id="text">
