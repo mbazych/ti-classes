5dfb0e326bc430e22ae9a8d9cb64c9d89687aa24
root <root@DESKTOP-B3KDVC4.localdomain>
2020-05-17 13:14:46 +0200

    add files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello wuja</title>
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+</head>
+<body id="main">
+    <p id="text">
+        Hello from Google Cloud!
+    <p>
+</body>
+</html>
